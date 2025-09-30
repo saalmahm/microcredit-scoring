79b31f50e3c1e6b64fa48ea6e1fdcd144be6d530
salma hamdi <saalmahm51@gmail.com>
2025-09-30 23:59:12 +0100

    Ajouter des valeurs et des méthodes à l'énumération TypeIncident

--- src/model/TypeIncident.java
+++ src/model/TypeIncident.java
@@ -1,5 +1,25 @@
 package model;
 
-public enum TypeIncident
-{
+public enum TypeIncident {
+    PAYE_A_TEMPS("Payé à Temps", 3),
+    EN_RETARD("En Retard", 0),
+    PAYE_EN_RETARD("Payé en Retard", 3),
+    IMPAYE_NON_REGLE("Impayé Non Réglé", -10),
+    IMPAYE_REGLE("Impayé Réglé", 5);
+
+    private String libelle;
+    private int impactScore;
+
+    TypeIncident(String libelle, int impactScore) {
+        this.libelle = libelle;
+        this.impactScore = impactScore;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public int getImpactScore() {
+        return impactScore;
+    }
 }
