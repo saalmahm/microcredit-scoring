a97cb97e792f8e4bfd40ac52206a9ded7adee097
salma hamdi <saalmahm51@gmail.com>
2025-09-30 23:58:58 +0100

    Ajouter des valeurs et des méthodes à l'énumération Decision

--- src/model/Decision.java
+++ src/model/Decision.java
@@ -1,4 +1,17 @@
 package model;
 
 public enum Decision {
+    ACCORDIMMEDIAT("Accord Immédiat"),
+    ETUDEMANUELLE("Étude Manuelle"),
+    REFUS_AUTOMATIQUE("Refus Automatique");
+
+    private String libelle;
+
+    Decision(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
 }
