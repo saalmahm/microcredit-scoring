278c1674f18acac2e42a7cb2013ec2f947b11726
salma hamdi <saalmahm51@gmail.com>
2025-10-01 12:19:58 +0100

    Ajouter des attributs et des méthodes à la classe Echeance

--- src/model/Echeance.java
+++ src/model/Echeance.java
@@ -1,4 +1,24 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Echeance {
+    private Integer id;
+    private LocalDate dateEcheance;
+    private Double mensualite;
+    private LocalDate datePaiement;
+    private StatusPaiment statutPaiement;
+
+    public Echeance() {}
+
+    public Echeance(Integer id, LocalDate dateEcheance, Double mensualite,
+                    LocalDate datePaiement, StatusPaiment statutPaiement) {
+        this.id = id;
+        this.dateEcheance = dateEcheance;
+        this.mensualite = mensualite;
+        this.datePaiement = datePaiement;
+        this.statutPaiement = statutPaiement;
+    }
+
+    // getters & setters
 }
