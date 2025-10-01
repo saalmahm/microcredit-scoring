5b92dbfe369cae8a9f8122ac51a75bc9078b1281
salma hamdi <saalmahm51@gmail.com>
2025-10-01 12:19:43 +0100

    Ajouter des attributs et des méthodes à la classe Employe

--- src/model/Employe.java
+++ src/model/Employe.java
@@ -1,4 +1,34 @@
 package model;
 
-public class Employe {
+public class Employe extends Personne {
+    private Double salaire;
+    private Integer anciennete;
+    private String poste;
+    private String typeContrat;
+    private Secteur secteur;
+
+    public Employe() {}
+
+    public Employe(Double salaire, Integer anciennete, String poste, String typeContrat, Secteur secteur) {
+        this.salaire = salaire;
+        this.anciennete = anciennete;
+        this.poste = poste;
+        this.typeContrat = typeContrat;
+        this.secteur = secteur;
+    }
+
+    public Double getSalaire() { return salaire; }
+    public void setSalaire(Double salaire) { this.salaire = salaire; }
+
+    public Integer getAnciennete() { return anciennete; }
+    public void setAnciennete(Integer anciennete) { this.anciennete = anciennete; }
+
+    public String getPoste() { return poste; }
+    public void setPoste(String poste) { this.poste = poste; }
+
+    public String getTypeContrat() { return typeContrat; }
+    public void setTypeContrat(String typeContrat) { this.typeContrat = typeContrat; }
+
+    public Secteur getSecteur() { return secteur; }
+    public void setSecteur(Secteur secteur) { this.secteur = secteur; }
 }
