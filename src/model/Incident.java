11f2380bf85834bf8704693fe267b286aa0a9ae7
salma hamdi <saalmahm51@gmail.com>
2025-10-01 12:20:11 +0100

    Ajouter des attributs et des méthodes à la classe Incident

--- src/model/Incident.java
+++ src/model/Incident.java
@@ -1,4 +1,24 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Incident {
+    private Integer id;
+    private LocalDate dateIncident;
+    private Echeance echeance;
+    private Double scoreImpact;
+    private TypeIncident typeIncident;
+
+    public Incident() {}
+
+    public Incident(Integer id, LocalDate dateIncident, Echeance echeance,
+                    Double scoreImpact, TypeIncident typeIncident) {
+        this.id = id;
+        this.dateIncident = dateIncident;
+        this.echeance = echeance;
+        this.scoreImpact = scoreImpact;
+        this.typeIncident = typeIncident;
+    }
+
+    // getters & setters
 }
