80c58acaccbfa6a2f22b3fa2286d3531b2f1afc9
salma hamdi <saalmahm51@gmail.com>
2025-10-01 12:20:47 +0100

    Ajouter des attributs et des méthodes à la classe Professionnel

--- src/model/Professionnel.java
+++ src/model/Professionnel.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Professionnel {
+public class Professionnel extends Personne {
+    private Double revenu;
+    private String immatriculationFiscale;
+    private Secteur secteurActivite;
+    private String activite;
+
+    public Professionnel() {}
+
+    public Professionnel(Double revenu, String immatriculationFiscale,
+                         Secteur secteurActivite, String activite) {
+        this.revenu = revenu;
+        this.immatriculationFiscale = immatriculationFiscale;
+        this.secteurActivite = secteurActivite;
+        this.activite = activite;
+    }
+
+    // getters & setters
 }
